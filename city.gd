705b7039be9e4b7ad41fb0c57e4119193bb28470
Alexander peczon <overbakedrice@gmail.com>
2025-03-01 14:55:06 -0800

    updated

--- city.gd
+++ city.gd
@@ -1,19 +1,32 @@
 extends Area3D
 
 func _ready():
-	# Connect the body_entered signal to our function
+	# Connect both enter and exit signals
 	body_entered.connect(_on_body_entered)
-	
-func _on_body_entered(body):
-	# Check if the body is a StaticBody3D that belongs to the camera
-	if body is StaticBody3D and is_camera_related(body):
-		print("Camera touched this hitbox!")
+	body_exited.connect(_on_body_exited)
+	area_entered.connect(_on_area_entered)
+	area_exited.connect(_on_area_exited)
 
-# Helper function to check if a node is related to the camera
+func _on_body_entered(body):
+	if is_camera_related(body):
+		print("entering ", name)
+	
+func _on_body_exited(body):
+	if is_camera_related(body):
+		print("leaving ", name)
+		
+func _on_area_entered(area):
+	if is_camera_related(area):
+		print("entering ", name)
+		
+func _on_area_exited(area):
+	if is_camera_related(area):
+		print("leaving ", name)
+		
 func is_camera_related(node):
 	var current = node
 	while current:
-		if current is Camera3D:
+		if current.name == "Camera3D":
 			return true
 		current = current.get_parent()
 	return false
